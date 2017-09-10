3d80b788d4ee9c417bbfa86dc6c6750e851da31c
Unknown <iceflower@gmx.de>
2017-09-11 00:00:17 +0200

    [enhancement] Add first shutdown part

--- mp3monitoring/gui/main.py
+++ mp3monitoring/gui/main.py
@@ -1,17 +1,25 @@
-from PyQt5.QtCore import Qt
+import threading
+
+import time
+from functools import partial
+
+from PyQt5.QtCore import Qt, QThread, QObject, pyqtSignal, pyqtSlot, QCoreApplication
 from PyQt5.QtWidgets import QMainWindow
 
 import core
 import gui.menu_items as menu
 from gui.menu_items import file, help, settings
+from gui.shutdown_worker import ShutdownWorker
 from gui.ui.main import Ui_MainWindow
-from gui.waiting_indicator import WaitingOverlay
+from gui.overlay import RotatingOverlay
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, app):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
+        self.app = app
 
         menu.file.set_item_actions(self)
         menu.settings.set_item_actions(self)
@@ -22,10 +30,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def closeEvent(self, event):  # TODO
         self.statusBar.showMessage('Shutting down...', 5000)
 
-        overlay = WaitingOverlay(parent=self.centralWidget)
+        overlay = RotatingOverlay(parent=self.centralWidget)
+        self.gridLayout.addWidget(overlay, 0, 0, 1, 1)
         overlay.resize(self.size())
         overlay.show()
-        core.shutdown()
+
+        self.shutdown_worker = ShutdownWorker()
+        self.thread = QThread()
+        self.shutdown_worker.status.connect(lambda s: print(s))
+        # 3 - Move the Worker object to the Thread object
+        self.shutdown_worker.moveToThread(self.thread)
+        # 5 - Connect Thread started signal to Worker operational slot method
+        self.thread.started.connect(self.shutdown_worker.shutdown)
+        # * - Thread finished signal will close the app if you want!
+        #self.thread.finished.connect(self.app.exit)
+        # 6 - Start the thread
+        self.thread.start()
 
         # TODO splash screen shutdown
         #event.accept()
---
+++ mp3monitoring/gui/overlay.py
@@ -0,0 +1,36 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+
+class RotatingOverlay(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(RotatingOverlay, self).__init__(parent, flags=Qt.FramelessWindowHint)
+
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.fill_color =  QtGui.QColor(30, 30, 30, 120)
+        self.text_color =  QtGui.QColor(255, 255, 255, 255)
+
+    def paintEvent(self, event):
+        # This method is, in practice, drawing the contents of
+        # your window.
+
+        # get current window size
+        size = self.size()
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
+        qp.setPen(self.fill_color)
+        qp.setBrush(self.fill_color)
+        qp.drawRect(0, 0, size.width(), size.height())
+
+        font = QtGui.QFont()
+        font.setPixelSize(18)
+        font.setBold(True)
+        qp.setFont(font)
+        qp.setPen(self.text_color)
+        ow = int(size.width() / 2)
+        oh = int(size.height() / 2)
+        qp.drawText(ow, oh, "Yep, I'm a pop up.")
+
+        qp.end()
