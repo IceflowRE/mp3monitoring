b68a5d6971d54e283f7d7a8c3f78157f639bdea6
Iceflower <iceflower@gmx.de>
2017-09-18 12:22:49 +0200

    [refactor] Change version

--- mp3monitoring/data/static.py
+++ mp3monitoring/data/static.py
@@ -1,7 +1,7 @@
 """
 Data which are constants and should be available global.
 """
-VERSION = '1.0.0'
+VERSION = '2017.09.18'
 NAME = 'MP3 Monitoring'
 AUTHOR = 'Iceflower S'
 AUTHOR_GITHUB = 'https://github.com/IceflowRE'
