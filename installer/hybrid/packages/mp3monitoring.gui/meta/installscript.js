d75b2541c898fa64ae0dd7da2d279e9af8dd837e
Iceflower <iceflower@gmx.de>
2017-10-05 17:32:56 +0200

    [installer] Add uninstaller for mp3monitoring

--- installer/hybrid/packages/mp3monitoring.gui/meta/installscript.js
+++ installer/hybrid/packages/mp3monitoring.gui/meta/installscript.js
@@ -6,4 +6,10 @@ Component.prototype.createOperations = function() {
     component.createOperations();
     
     component.addOperation("Execute", "pip install --upgrade", "pyqt5");
+    component.addOperation("CreateShortcut", "@TargetDir@/README.md", "@StartMenuDir@/MP3 Monitoring Gui.lnk",
+                           "workingDirectory=@TargetDir@", "iconPath=%SystemRoot%/system32/SHELL32.dll",
+                           "iconId=2", "description=Start the MP3 Monitoring Gui");
+    component.addOperation("CreateShortcut", "@TargetDir@/README.md", "@TargetDir@/MP3 Monitoring Gui.lnk",
+                           "workingDirectory=@TargetDir@", "iconPath=%SystemRoot%/system32/SHELL32.dll",
+                           "iconId=2", "description=Start the MP3 Monitoring Gui");
 }
--- installer/hybrid/packages/mp3monitoring/meta/installscript.js
+++ installer/hybrid/packages/mp3monitoring/meta/installscript.js
@@ -18,7 +18,7 @@ Component.prototype.createOperations = function() {
     component.createOperations();
     
     component.addOperation("Copy", tmpDir + "README.md", "@TargetDir@")
-    component.addOperation("Execute", "pip", "install", "--no-deps", tmpDir + wheelName);
+    component.addOperation("Execute", "pip", "install", "--no-deps", tmpDir + wheelName, "UNDOEXECUTE", "pip", "uninstall", "-y", "mp3monitoring");
     component.addOperation("Execute", "pip", "install", "--upgrade", "mutagen");
     component.addOperation("Execute", "pip", "install", "--upgrade", "tqdm");
 }
