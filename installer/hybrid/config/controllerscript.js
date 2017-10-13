e1538bd80933588c200a27353c92f360e8edec12
Iceflower <iceflower@gmx.de>
2017-10-13 14:46:56 +0200

    [installer] Move pre check into controller

--- installer/hybrid/config/controllerscript.js
+++ installer/hybrid/config/controllerscript.js
@@ -3,6 +3,7 @@ function Controller() {
 }
 
 Controller.prototype.IntroductionPageCallback = function() {
+    checkPrerequisite();
     // disable updater radio buttons
     if (installer.isUninstaller()) {
         var widget = gui.currentPageWidget();
@@ -12,10 +13,69 @@ Controller.prototype.IntroductionPageCallback = function() {
     }
 }
 
+
+
 Controller.prototype.ReadyForInstallationPageCallback = function() {
     if (installer.isUninstaller()) {
         var widget = gui.currentPageWidget(); 
-        pythonText = "<font color='red'>\n This will uninstall the following python packages if the associated component is installed: tqdm, mutagen - pyqt5 (gui) - urllib3 (updater)!</font>";
+        pythonText = "<br><br><font color='red'>\n This will uninstall the following python packages if the associated component is installed: tqdm, mutagen - pyqt5 (gui) - urllib3 (updater)!</font>";
         widget.findChild("MessageLabel").setText(widget.findChild("MessageLabel").text + pythonText);
     }
+}
+
+var tmpDir = "@RootDir@/tmp/mp3monitoring/";
+var checkPythonArr = new Array("", "if command -v python; then", "    exit 0", "else", "    exit 1", "fi");
+var checkPipArr = new Array("", "if command -v pip; then", "    exit 0", "else", "    exit 1", "fi");
+
+checkPrerequisite = function() {
+    if (installer.value("os") != "win") {
+        cancelInstaller("Installation on " + systemInfo.prettyProductName + " is not supported");
+        return;
+    }
+    var windir = installer.environmentVariable("WINDIR");
+    if (windir == "") {
+        cancelInstaller("Windows installation path not detected.");
+        return;
+    }
+    var cmdLocation = windir + "\\system32\\cmd.exe";
+    
+    // check python
+    var file = tmpDir + "check_python.sh";
+    installer.execute(cmdLocation, new Array("/C", "echo #!/bin/sh > " + file));
+    for (i = 1; i < checkPythonArr.length; i++) {
+        installer.execute(cmdLocation, new Array("/C", "echo " + checkPythonArr[i] + " >> " + file));
+    }
+    var exists = installer.execute(cmdLocation, new Array("/C", "bash " + file))[1];
+    if (exists != 0) {
+        cancelInstaller("No Python installation detected.");
+        return;
+    } else {
+        console.log("Check: Python installation detected.");
+    }
+    
+    // check pip
+    file = tmpDir + "check_pip.sh"
+    installer.execute(cmdLocation, new Array("/C", "echo #!/bin/sh > " + file));
+    for (i = 1; i < checkPipArr.length; i++) {
+        installer.execute(cmdLocation, new Array("/C", "echo " + checkPipArr[i] + " >> " + file));
+    }
+    var exists = installer.execute(cmdLocation, new Array("/C", "bash " + file))[1];
+    if (exists != 0) {
+        cancelInstaller("No Pip installation detected.");
+        return;
+    } else {
+        console.log("Check: Pip installation detected.");
+    }
+}
+
+cancelInstaller = function(message) {
+    var abortText = "<font size='4'><font color='red'>" + message + "</font></font>";
+    installer.setValue("FinishedText", abortText);
+    installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
+    installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, false);
+    installer.setDefaultPageVisible(QInstaller.ComponentSelection, false);
+    installer.setDefaultPageVisible(QInstaller.StartMenuSelection, false);
+    installer.setDefaultPageVisible(QInstaller.PerformInstallation, false);
+    installer.setDefaultPageVisible(QInstaller.LicenseCheck, false);
+    gui.clickButton(buttons.NextButton);
 }
--- installer/hybrid/packages/mp3monitoring/meta/installscript.js
+++ installer/hybrid/packages/mp3monitoring/meta/installscript.js
@@ -2,11 +2,7 @@ var tmpDir = "@RootDir@/tmp/mp3monitoring/";
 var wheelName = "MP3_Monitoring-1.0.0-py3-none-any.whl";
 
 function Component() {
-    if (installer.value("os") != "win") {
-        cancelInstaller("Installation on " + systemInfo.prettyProductName + " is not supported");
-        return;
-    }
-    checkPrerequisite();
+    
 }
 
 // extract data to tmp dir
@@ -21,54 +17,4 @@ Component.prototype.createOperations = function() {
     component.addOperation("Execute", "pip", "install", "--no-deps", tmpDir + wheelName, "UNDOEXECUTE", "pip", "uninstall", "-y", "mp3-monitoring");
     component.addOperation("Execute", "pip", "install", "--upgrade", "mutagen", "UNDOEXECUTE", "pip", "uninstall", "-y", "mutagen");
     component.addOperation("Execute", "pip", "install", "--upgrade", "tqdm", "UNDOEXECUTE", "pip", "uninstall", "-y", "tqdm");
-}
-
-var checkPythonArr = new Array("", "if command -v python; then", "    exit 0", "else", "    exit 1", "fi");
-var checkPipArr = new Array("", "if command -v pip; then", "    exit 0", "else", "    exit 1", "fi");
-
-function checkPrerequisite() {
-    var windir = installer.environmentVariable("WINDIR");
-    if (windir == "") {
-        cancelInstaller("Windows installation path not detected.");
-    }
-    var cmdLocation = windir + "\\system32\\cmd.exe";
-    
-    // check python
-    var file = "@RootDir@/tmp/check_python.sh";
-    installer.execute(cmdLocation, new Array("/C", "echo #!/bin/sh > " + file));
-    for (i = 1; i < checkPythonArr.length; i++) {
-        installer.execute(cmdLocation, new Array("/C", "echo " + checkPythonArr[i] + " >> " + file));
-    }
-    var exists = installer.execute(cmdLocation, new Array("/C", "bash " + file))[1];
-    if (exists != 0) {
-        cancelInstaller("No Python installation detected.");
-    } else {
-        console.log("Check: Python installation detected.");
-    }
-    
-    // check pip
-    file = "@RootDir@/tmp/check_pip.sh"
-    installer.execute(cmdLocation, new Array("/C", "echo #!/bin/sh > " + file));
-    for (i = 1; i < checkPipArr.length; i++) {
-        installer.execute(cmdLocation, new Array("/C", "echo " + checkPipArr[i] + " >> " + file));
-    }
-    var exists = installer.execute(cmdLocation, new Array("/C", "bash " + file))[1];
-    if (exists != 0) {
-        cancelInstaller("No Pip installation detected.");
-    } else {
-        console.log("Check: Pip installation detected.");
-    }
-}
-
-function cancelInstaller(message) {
-    installer.setDefaultPageVisible(QInstaller.Introduction, false);
-    installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
-    installer.setDefaultPageVisible(QInstaller.ComponentSelection, false);
-    installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, false);
-    installer.setDefaultPageVisible(QInstaller.StartMenuSelection, false);
-    installer.setDefaultPageVisible(QInstaller.PerformInstallation, false);
-    installer.setDefaultPageVisible(QInstaller.LicenseCheck, false);
-
-    var abortText = "<font size='4'><font color='red'>" + message + "</font></font>";
-    installer.setValue("FinishedText", abortText);
 }
