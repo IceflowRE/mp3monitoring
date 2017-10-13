976a9919e0620989f130bfdd9c55d033569e68d9
Iceflower <iceflower@gmx.de>
2017-10-13 15:28:50 +0200

    [code] Prepare next version

--- mp3monitoring/data/static.py
+++ mp3monitoring/data/static.py
@@ -1,7 +1,7 @@
 """
 Data which are constants and should be available global.
 """
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 NAME = 'MP3-Monitoring'
 AUTHOR = 'Iceflower S'
 AUTHOR_GITHUB = 'https://github.com/IceflowRE'
