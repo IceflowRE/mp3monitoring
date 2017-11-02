0a8808bd6371c302485dbf71d1313d2dd2a53a7b
Unknown <iceflower@gmx.de>
2017-11-02 20:45:56 +0100

    [installer] Fix small bugs

--- scripts/create_installer.sh
+++ scripts/create_installer.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 # executed from project root
 
-cd installer
-cur_folder="./hybrid/config/data"
+cur_folder="./installer/hybrid/config/data"
 rm -rf ${cur_folder}
 mkdir -p ${cur_folder}
-inkscape ../mp3monitoring/pkg_data/gui/icon_export.svg --export-png=${cur_folder}/icon.png -w512 -h512
+inkscape ./mp3monitoring/pkg_data/gui/icon_export.svg --export-png=${cur_folder}/icon.png -w512 -h512
 
-cur_folder="./hybrid/packages/mp3monitoring/data/"
+cur_folder="./installer/hybrid/packages/mp3monitoring/data/"
 rm -rf ${cur_folder}
 mkdir -p ${cur_folder}
-cp ../README.md ${cur_folder}/README.md
-cp ../dist/MP3_Monitoring-1.0.1-py36-none-any.whl ${cur_folder}/MP3_Monitoring-1.0.1-py36-none-any.whl
+cp ./README.md ${cur_folder}/README.md
+cp ./dist/MP3_Monitoring-1.0.2-py36-none-any.whl ${cur_folder}/MP3_Monitoring-1.0.2-py36-none-any.whl
 
-cur_folder="./hybrid/packages/mp3monitoring.gui/data/"
+cur_folder="./installer/hybrid/packages/mp3monitoring.gui/data/"
 rm -rf ${cur_folder}
 mkdir -p ${cur_folder}
-cp ./icon.ico ${cur_folder}/icon.ico
+cp ./installer/icon.ico ${cur_folder}/icon.ico
 
-cur_folder="./hybrid/packages/mp3monitoring/meta/"
-cp ../LICENSE.md ${cur_folder}/LICENSE.md
+cur_folder="./installer/hybrid/packages/mp3monitoring/meta/"
+cp ./LICENSE.md ${cur_folder}/LICENSE.md
 
 mkdir -p bin
-binarycreator -c ./hybrid/config/config.xml -p ./hybrid/packages/ --offline-only "./bin/MP3 Monitoring Hybrid Setup.exe"
+binarycreator -c ./installer/hybrid/config/config.xml -p ./installer/hybrid/packages/ --offline-only "./installer/bin/MP3 Monitoring Hybrid Setup.exe"
 if [ $? -ne 0 ]; then
     exit 1
 fi
-cd ..
 
 exit 0
