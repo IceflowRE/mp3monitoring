f27d8d56a016292694d9579017e7f4d2c49346e2
Unknown <iceflower@gmx.de>
2017-11-05 21:49:48 +0100

    [release] 1.0.3

--- mp3monitoring/data/static.py
+++ mp3monitoring/data/static.py
@@ -1,7 +1,7 @@
 """
 Data which are constants and should be available global.
 """
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 NAME = 'MP3-Monitoring'
 AUTHOR = 'Iceflower S'
 AUTHOR_GITHUB = 'https://github.com/IceflowRE'
--- scripts/create_installer.sh
+++ scripts/create_installer.sh
@@ -10,7 +10,7 @@ cur_folder="./installer/hybrid/packages/mp3monitoring/data/"
 rm -rf ${cur_folder}
 mkdir -p ${cur_folder}
 cp ./README.md ${cur_folder}/README.md
-cp ./dist/MP3_Monitoring-1.0.2-py36-none-any.whl ${cur_folder}/MP3_Monitoring-1.0.2-py36-none-any.whl
+cp ./dist/MP3_Monitoring-1.0.3-py36-none-any.whl ${cur_folder}/MP3_Monitoring-1.0.3-py36-none-any.whl
 
 cur_folder="./installer/hybrid/packages/mp3monitoring.gui/data/"
 rm -rf ${cur_folder}
@@ -21,7 +21,7 @@ cur_folder="./installer/hybrid/packages/mp3monitoring/meta/"
 cp ./LICENSE.md ${cur_folder}/LICENSE.md
 
 mkdir -p bin
-binarycreator -c ./installer/hybrid/config/config.xml -p ./installer/hybrid/packages/ --offline-only "./installer/bin/MP3 Monitoring Hybrid Setup.exe"
+binarycreator -c ./installer/hybrid/config/config.xml -p ./installer/hybrid/packages/ --offline-only "./installer/bin/MP3 Monitoring Hybrid Setup 1_0_3.exe"
 if [ $? -ne 0 ]; then
     exit 1
 fi
