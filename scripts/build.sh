b08c2746583a80cbe7b8896510c575532da3b058
Unknown <iceflower@gmx.de>
2017-11-06 23:00:56 +0100

    [build] Get rid of the version naming

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # executed from project root
 
+py_version="$1"
+
 ./scripts/qt_ui_to_py.sh
 
 python setup.py clean --all
-python setup.py bdist_wheel --python-tag "$1"
+python setup.py bdist_wheel --python-tag "$py_version"
 
-./scripts/create_installer.sh
+app_version=$(grep -oP "VERSION\s=\s'\K\w+.\w+.\w+" ./mp3monitoring/data/static.py)
+./scripts/create_installer.sh "$py_version" "$app_version"
--- scripts/create_installer.sh
+++ scripts/create_installer.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # executed from project root
 
+py_version="$1"
+app_version="$2"
+
 cur_folder="./installer/hybrid/config/data"
 rm -rf ${cur_folder}
 mkdir -p ${cur_folder}
@@ -10,7 +13,7 @@ cur_folder="./installer/hybrid/packages/mp3monitoring/data/"
 rm -rf ${cur_folder}
 mkdir -p ${cur_folder}
 cp ./README.md ${cur_folder}/README.md
-cp ./dist/MP3_Monitoring-1.0.3-py36-none-any.whl ${cur_folder}/MP3_Monitoring-1.0.3-py36-none-any.whl
+cp ./dist/MP3_Monitoring-"$app_version"-"$py_version"-none-any.whl ${cur_folder}/MP3_Monitoring-"$app_version"-"$py_version"-none-any.whl
 
 cur_folder="./installer/hybrid/packages/mp3monitoring.gui/data/"
 rm -rf ${cur_folder}
@@ -21,7 +24,7 @@ cur_folder="./installer/hybrid/packages/mp3monitoring/meta/"
 cp ./LICENSE.md ${cur_folder}/LICENSE.md
 
 mkdir -p bin
-binarycreator -c ./installer/hybrid/config/config.xml -p ./installer/hybrid/packages/ --offline-only "./installer/bin/MP3 Monitoring Hybrid Setup 1_0_3.exe"
+binarycreator -c ./installer/hybrid/config/config.xml -p ./installer/hybrid/packages/ --offline-only "./installer/bin/MP3 Monitoring Hybrid Setup ""${app_version//[.]/_}"".exe"
 if [ $? -ne 0 ]; then
     exit 1
 fi
