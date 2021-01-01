3ace7d707d1bd36c17514ec0e45a93131cdc6c62
Iceflower <iceflower@gmx.de>
2021-01-01 17:54:02 +0100

    [job] Remove test waiting time

--- mp3monitoring/core/job.py
+++ mp3monitoring/core/job.py
@@ -106,7 +106,6 @@ class Job:
                 self.status = 'ok'
             if self._sleep_event.wait(self.config.sleep_time):
                 self._sleep_event.clear()
-                time.sleep(10)
                 self._active = False
                 self.status = 'stopped'
                 return
